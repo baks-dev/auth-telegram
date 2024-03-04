86f2bd27357cdb54683f9ade3a388ae6f0d89ca7
kingstar <admin@baks.dev>
2024-03-04 16:40:25 +0300

    Baks Development

--- Controller/Admin/IndexController.php
+++ Controller/Admin/IndexController.php
@@ -50,7 +50,9 @@ final class IndexController extends AbstractController
 
         // Поиск
         $search = new SearchDTO();
-        $searchForm = $this->createForm(SearchForm::class, $search);
+        $searchForm = $this->createForm(SearchForm::class, $search,
+            ['action' => $this->generateUrl('auth-telegram:admin.index')]
+        );
         $searchForm->handleRequest($request);
 
         // Фильтр
