46f0191bcbd33dda91554747dbe938a76d80e30f
kingstar <admin@baks.dev>
2024-03-14 16:34:24 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -44,6 +44,8 @@ return static function (FrameworkConfig $framework) {
     ;
 
     $messenger->transport('failed-auth-telegram')
-        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%');
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
+        ->options(['queue_name' => 'failed-auth-telegram'])
+    ;
 
 };
