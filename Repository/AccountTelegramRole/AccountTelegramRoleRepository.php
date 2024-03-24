5aa7d4be930b3cde5a9a26fceae400723de3f747
kingstar <admin@baks.dev>
2024-03-24 16:38:52 +0300

    Baks Development

---
+++ Repository/AccountTelegramRole/AccountTelegramRoleRepository.php
@@ -0,0 +1,150 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Auth\Telegram\Repository\AccountTelegramRole;
+
+use BaksDev\Auth\Telegram\Entity\AccountTelegram;
+use BaksDev\Auth\Telegram\Entity\Event\AccountTelegramEvent;
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Users\Profile\Group\BaksDevUsersProfileGroupBundle;
+use BaksDev\Users\Profile\Group\Entity\Event\ProfileGroupEvent;
+use BaksDev\Users\Profile\Group\Entity\ProfileGroup;
+use BaksDev\Users\Profile\Group\Entity\Role\ProfileRole;
+use BaksDev\Users\Profile\Group\Entity\Role\Voter\ProfileVoter;
+use BaksDev\Users\Profile\Group\Entity\Users\ProfileGroupUsers;
+use BaksDev\Users\Profile\UserProfile\Entity\Info\UserProfileInfo;
+use BaksDev\Users\Profile\UserProfile\Entity\UserProfile;
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
+use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\Status\UserProfileStatusActive;
+use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\UserProfileStatus;
+
+final class AccountTelegramRoleRepository implements AccountTelegramRoleInterface
+{
+    private DBALQueryBuilder $DBALQueryBuilder;
+
+    public function __construct(
+        DBALQueryBuilder $DBALQueryBuilder,
+    )
+    {
+        $this->DBALQueryBuilder = $DBALQueryBuilder;
+    }
+
+    /**
+     * Метод возвращает всех пользователей Telegram, имеющие доступ (РОЛЬ) к профилю
+     */
+    public function fetchAll(UserProfileUid $profile, string $role): array|bool
+    {
+        if(!class_exists(BaksDevUsersProfileGroupBundle::class))
+        {
+            return false;
+        }
+
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->from(ProfileGroupUsers::class, 'usr')
+            ->where('usr.profile = :profile OR usr.authority = :profile')
+            ->setParameter('profile', $profile, UserProfileUid::TYPE);
+
+
+
+        $dbal
+            ->join(
+                'usr',
+                ProfileGroup::class,
+                'grp',
+                'grp.prefix = usr.prefix'
+            );
+
+        $dbal
+            ->join(
+                'grp',
+                ProfileRole::class,
+                'role',
+                'role.event = grp.event'
+            );
+
+        $dbal
+            ->join(
+                'role',
+                ProfileVoter::class,
+                'voter',
+                'voter.role = role.id'
+            );
+
+        $dbal
+            ->andWhere('(role.prefix = :role OR voter.prefix = :role)')
+            ->setParameter('role', $role);
+
+
+        /** UserProfile */
+
+        $dbal
+            ->join(
+                'usr',
+                UserProfileInfo::class,
+                'profile_info',
+                '
+                profile_info.profile = usr.profile AND 
+                profile_info.status = :profile_status AND 
+                profile_info.active = true
+            ')
+            ->setParameter('profile_status', new UserProfileStatus(UserProfileStatusActive::class), UserProfileStatus::TYPE);
+
+
+        $dbal
+            ->join(
+                'profile_info',
+                UserProfile::class,
+                'profile',
+                'profile.id = profile_info.profile'
+            );
+
+
+        /** Account Telegram */
+        $dbal
+            ->join(
+                'profile_info',
+                AccountTelegram::class,
+                'account',
+                'account.id = profile_info.usr'
+            );
+
+        /** Account Telegram */
+        $dbal
+            ->select('account_event.chat')
+            ->leftJoin(
+                'account',
+                AccountTelegramEvent::class,
+                'account_event',
+                'account_event.id = account.event'
+            );
+
+
+        return $dbal
+            ->enableCache('auth-telegram', 3600)
+            ->fetchAllAssociative();
+    }
+}
---
+++ Repository/ActiveUserTelegramAccount/ActiveUserTelegramAccountRepository.php
@@ -0,0 +1,171 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Auth\Telegram\Repository\ActiveUserTelegramAccount;
+
+use BaksDev\Auth\Telegram\Entity\AccountTelegram;
+use BaksDev\Auth\Telegram\Entity\Event\AccountTelegramEvent;
+use BaksDev\Auth\Telegram\Type\Event\AccountTelegramEventUid;
+use BaksDev\Auth\Telegram\Type\Status\AccountTelegramStatus;
+use BaksDev\Auth\Telegram\Type\Status\AccountTelegramStatus\AccountTelegramStatusBlock;
+use BaksDev\Auth\Telegram\Type\Status\AccountTelegramStatus\AccountTelegramStatusActive;
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Users\Profile\UserProfile\Entity\Info\UserProfileInfo;
+use BaksDev\Users\Profile\UserProfile\Entity\UserProfile;
+use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\Status\UserProfileStatusActive;
+use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\UserProfileStatus;
+use BaksDev\Users\User\Type\Id\UserUid;
+
+final class ActiveUserTelegramAccountRepository implements ActiveUserTelegramAccountInterface
+{
+    private DBALQueryBuilder $DBALQueryBuilder;
+
+    public function __construct(
+        DBALQueryBuilder $DBALQueryBuilder,
+    )
+    {
+        $this->DBALQueryBuilder = $DBALQueryBuilder;
+    }
+
+    /**
+     * Метод возвращает идентификатор пользователя UserUid по идентификатору чата
+     */
+    public function findByChat(int|string $chat): ?UserUid
+    {
+
+        $chat = (string) $chat;
+
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->select('event.account')
+            ->from(AccountTelegramEvent::class, 'event');
+
+        $dbal->where('event.chat = :chat')
+            ->setParameter('chat', $chat);
+
+        $dbal
+            ->andWhere('event.status = :telegram_status')
+            ->setParameter(
+                'telegram_status',
+                new AccountTelegramStatus(AccountTelegramStatusActive::class),
+                AccountTelegramStatus::TYPE);
+
+        $dbal->join(
+            'event',
+            AccountTelegram::class,
+            'main',
+            'main.event = event.id'
+        );
+
+        $exist = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $exist->select('profile_info.profile');
+        $exist->from(UserProfileInfo::class, 'profile_info');
+        $exist->where('profile_info.usr = event.account');
+        $exist->andWhere('profile_info.status = :profile_status');
+        $exist->andWhere('profile_info.active = true');
+
+        $exist->join(
+            'profile_info',
+            UserProfile::class,
+            'profile',
+            'profile.id = profile_info.profile');
+
+
+        $dbal->setParameter('profile_status',
+            new UserProfileStatus(UserProfileStatusActive::class),
+            UserProfileStatus::TYPE);
+
+
+        $dbal->andWhere('EXISTS('.$exist->getSQL().')');
+
+        $result = $dbal->enableCache('auth-telegram', 3600)->fetchOne();
+
+        return $result ? new UserUid($result) : null;
+    }
+
+
+    /**
+     * Метод возвращает идентификатор пользователя по активному идентификатору события
+     */
+    public function findByEvent(AccountTelegramEventUid|string $event): ?UserUid
+    {
+        if(is_string($event))
+        {
+            $event = new AccountTelegramEventUid($event);
+        }
+
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->select('event.account')
+            ->from(AccountTelegramEvent::class, 'event');
+
+        $dbal->where('event.id = :event')
+            ->setParameter('event', $event, AccountTelegramEventUid::TYPE);
+
+        $dbal->andWhere('event.status != :telegram_status')
+            ->setParameter(
+                'telegram_status',
+                new AccountTelegramStatus(new AccountTelegramStatusBlock()),
+                AccountTelegramStatus::TYPE);
+
+        $dbal->join(
+            'event',
+            AccountTelegram::class,
+            'main',
+            'main.event = event.id'
+        );
+
+        /**  Проверяем, что имеется активный профиль */
+//        $exist = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+//
+//        $exist->select('1');
+//        $exist->from(UserProfileInfo::class, 'profile_info');
+//        $exist->where('profile_info.usr = event.account');
+//        $exist->andWhere('profile_info.status = :profile_status');
+//        $exist->andWhere('profile_info.active = true');
+//
+//        $exist->join(
+//            'profile_info',
+//            UserProfile::class,
+//            'profile',
+//            'profile.id = profile_info.profile');
+//
+//
+//        $dbal->setParameter('profile_status',
+//            new UserProfileStatus(new UserProfileStatusActive()),
+//            UserProfileStatus::TYPE);
+//
+//        $dbal->andWhere('EXISTS('.$exist->getSQL().')');
+
+
+        $result = $dbal->enableCache('auth-telegram', 3600)->fetchOne();
+
+        return $result ? new UserUid($result) : null;
+    }
+
+}
