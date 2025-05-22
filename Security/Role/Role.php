e49e96d4c7cc27e64a1f05fb42b0268baf11917a
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-22 07:23:33 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Security/Role/Role.php
+++ Security/Role/Role.php
@@ -35,7 +35,9 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 #[AutoconfigureTag('baks.menu.admin')]
 final class Role implements RoleInterface, MenuAdminInterface
 {
-    public const ROLE = 'ROLE_ACCOUNT_TELEGRAM';
+    public const string ROLE = 'ROLE_ACCOUNT_TELEGRAM';
+
+    public const string KEY = 'srXAuXrdz';
 
     public function getRole(): string
     {
@@ -50,6 +52,14 @@ final class Role implements RoleInterface, MenuAdminInterface
     public function getPath(): string
     {
         return 'auth-telegram:admin.index';
+    }
+
+    /**
+     * Метод возвращает ключ раздела (для меню телеграм)
+     */
+    public function getPathKey(): string
+    {
+        return self::KEY;
     }
 
     /** Метод возвращает секцию, в которую помещается ссылка на раздел */
